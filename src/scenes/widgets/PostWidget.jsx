ab64cdb40bea855b273a63eb23b4c7829f5c5b4c
Roberto <rcastellong98@gmail.com>
2023-03-16 22:48:48 +0100

    [bugFix]

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -52,7 +52,7 @@ const PostWidget = ({
           height={'auto'}
           alt='post'
           style={{ borderRadius: '0.75rem', marginTop: '0.75' }}
-          src={`http://localhost:3001/assets/${picturePath}`}
+          src={`https://social-media-server.vercel.app/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt={'0.25'}>
