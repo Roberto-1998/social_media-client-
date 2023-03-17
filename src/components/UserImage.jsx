6e87f13ea1d3a95493aa0b0d7c718457c5f51044
Roberto <rcastellong98@gmail.com>
2023-03-17 01:02:55 +0100

    [bugFix] image

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = '60px' }) => {
         width={size}
         height={size}
         alt='user'
-        src={`../assets/${image}`}
+        src={`https://social-media-server.vercel.app/assets/${image}`}
       />
     </Box>
   );
